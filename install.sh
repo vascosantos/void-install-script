636bba950684a9ac55bfd4a2b9262d745e969f32
Vasco Santos <vasco.f.santos@gmail.com>
2025-03-12 20:58:38 +0000

    changed to ZFS

--- install.sh
+++ install.sh
@@ -10,8 +10,11 @@
 #
 
 # Global variables
-DISK=/dev/vda
-BTRFS_OPT=rw,noatime,ssd,discard=async,compress-force=zstd:1,space_cache=v2,commit=120
+BOOT_DISK=/dev/vda
+POOL_DISK=/dev/vda
+BOOT_PARTITION=1
+POOL_PARTITION=2
+# BTRFS_OPT=rw,noatime,ssd,discard=async,compress-force=zstd:1,space_cache=v2,commit=120
 KEYMAP=us-acentos
 TIME_ZONE=Europe/Lisbon
 ARCH=x86_64
@@ -22,46 +25,69 @@ ZRAM_COMPRESSOR=zstd    # zram compression algorithm (see https://github.com/atw
 ZRAM_INIT_SIZE_PCT=2    # initial zram size as a percentage of total RAM
 ZRAM_MAX_SIZE_MB=16384  # maximum zram size in MB
 
+# detect if BOOT_DISK contains "nvme"
+if [[ $BOOT_DISK == *"nvme"* ]]; then
+  BOOT_DEVICE="${BOOT_DISK}p${BOOT_PARTITION}"
+else
+  BOOT_DEVICE="${BOOT_DISK}${BOOT_PARTITION}"
+fi
+if [[ $POOL_DISK == *"nvme"* ]]; then
+  POOL_DEVICE="${POOL_DISK}p${POOL_PARTITION}"
+else
+  POOL_DEVICE="${POOL_DISK}${POOL_PARTITION}"
+fi
+
+# Generate /etc/hostid
+ID="void"
+zgenhostid -f 0x00bab10c
+
+# Wipe all partitions
+zpool labelclear -f "$POOL_DISK"
+wipefs -a "$POOL_DISK"
+wipefs -a "$BOOT_DISK"
+sgdisk --zap-all "$POOL_DISK"
+sgdisk --zap-all "$BOOT_DISK"
+
+# Create partitions
+sgdisk -n "${BOOT_PARTITION}:1m:+512m" -t "${BOOT_PART}:ef00" "$BOOT_DISK"
+sgdisk -n "${POOL_PARTITION}:0:-10m" -t "${POOL_PART}:bf00" "$POOL_DISK"
+
+# Create the zpool
+zpool create -f -o ashift=12 \
+    -O compression=lz4 \
+    -O acltype=posixacl \
+    -O xattr=sa \
+    -O relatime=on \
+    -o autotrim=on \
+    -o compatibility=openzfs-2.3.1-linux \
+    -m none zroot "$POOL_DEVICE"
+
+# Create initial file systems
+zfs create -o mountpoint=none zroot/ROOT
+zfs create -o mountpoint=/ -o canmount=noauto zroot/ROOT/${ID}
+zfs create -o mountpoint=/home zroot/home
+zpool set bootfs=zroot/ROOT/${ID} zroot
+
+# Export and re-import the pool to mount the root filesystem
+zpool export zroot
+zpool import -N -R /mnt zroot
+zfs mount zroot/ROOT/${ID}
+zfs mount zroot/home
+
+# Update device symlinks
+udevadm trigger
+
 # Update Void
 xbps-install -Suy   # might need to run twice to ensure all packages are up-to-date
-xbps-install -Suy
-
-# Install gptfdisk on the live system
-xbps-install -Sy gptfdisk
-
-# Perform disk partitioning - ALL CONTENTS WILL BE LOST
-sgdisk -Z ${DISK}
-sgdisk -a 2048 -o ${DISK}
-sgdisk -n 1:0:+512M ${DISK} # /dev/sda1
-sgdisk -n 2:0:0 ${DISK}     # /dev/sda2
-sgdisk -t 1:ef00 ${DISK} 
-sgdisk -t 2:8300 ${DISK} 
-mkfs.vfat -F 32 -n EFI ${DISK}1
-mkfs.btrfs -L Void -f ${DISK}2
-
-# Mount partitions and create subvolumes
-mount -o $BTRFS_OPT ${DISK}2 /mnt
-btrfs subvolume create /mnt/@
-btrfs subvolume create /mnt/@home
-btrfs subvolume create /mnt/@snapshots
-umount /mnt
-mount -o $BTRFS_OPT,subvol=@ ${DISK}2 /mnt
-mkdir /mnt/home
-mkdir /mnt/.snapshots
-mount -o $BTRFS_OPT,subvol=@home ${DISK}2 /mnt/home/
-mkdir -p /mnt/boot/efi
-mount -o rw,noatime ${DISK}1 /mnt/boot/efi/
-mkdir -p /mnt/var/cache
-btrfs subvolume create /mnt/var/cache/xbps
-btrfs subvolume create /mnt/var/tmp
-btrfs subvolume create /mnt/var/log
+xbps-install -Suy 
 
 # Install base system and some basic packages
-XBPS_ARCH=$ARCH xbps-install -Sy -r /mnt -R "$REPO/current" base-system btrfs-progs grub-x86_64-efi grub-btrfs grub-btrfs-runit NetworkManager zramen
+XBPS_ARCH=$ARCH xbps-install -Sy -r /mnt -R "$REPO/current" base-system zfs zfsbootmenu systemd-boot-efistub refind zramen
 for dir in sys dev proc; do 
     mount --rbind /$dir /mnt/$dir; mount --make-rslave /mnt/$dir; 
 done
 cp -L /etc/resolv.conf /mnt/etc/
+cp -L /etc/hostid /mnt/etc/
 
 # Set hostname, timezone and locales
 echo $HOST_NAME > /mnt/etc/hostname
@@ -93,19 +119,6 @@ echo "%wheel ALL=(ALL) ALL" > /mnt/etc/sudoers.d/10-wheel.conf
 echo "%wheel ALL= (ALL) NOPASSWD: /sbin/shutdown, /sbin/reboot, /sbin/halt" >> /mnt/etc/sudoers.d/10-wheel.conf # allow shutdown, reboot and halt without password
 echo "Defaults timestamp_timeout=60" >> /mnt/etc/sudoers.d/10-wheel.conf    # require password only every 60 minutes (default is 5)
 
-# Write fstab
-export BOOT_UUID=$(blkid -s UUID -o value ${DISK}1)
-export ROOT_UUID=$(blkid -s UUID -o value ${DISK}2)
-
-cat <<EOFSTAB >> /mnt/etc/fstab
-UUID=$BOOT_UUID /boot/efi   vfat    defaults                        0 2
-UUID=$ROOT_UUID /           btrfs   $BTRFS_OPT,subvol=@             0 0
-UUID=$ROOT_UUID /home       btrfs   $BTRFS_OPT,subvol=@home         0 0
-UUID=$ROOT_UUID /.snapshots btrfs   $BTRFS_OPT,subvol=@snapshots    0 0
-UUID=$ROOT_UUID /var/log    btrfs   $BTRFS_OPT,subvol=@/var/log     0 0
-tmpfs           /tmp        tmpfs   defaults,noatime,mode=1777      0 0
-EOFSTAB
-
 # Configure zram swap
 sed -i "s/.*ZRAM_COMP_ALGORITHM.*/export ZRAM_COMP_ALGORITHM=$ZRAM_COMPRESSOR/g" /mnt/etc/sv/zramen/conf
 sed -i "s/.*ZRAM_SIZE.*/export ZRAM_SIZE=$ZRAM_INIT_SIZE_PCT/g" /mnt/etc/sv/zramen/conf
@@ -123,14 +136,17 @@ sed -i "s|https://repo-default.voidlinux.org|$REPO|g" /mnt/etc/xbps.d/*-reposito
 # Install intel-ucode and nvidia drivers
 XBPS_ARCH=$ARCH xbps-install -r /mnt -Sy intel-ucode mesa-dri nvidia
 
-# Configure nvidia, dracut and efibootmgr
+# Configure nvidia, dracut and rEFInd
 cat <<EOMODPROBENVIDIACONF >> /mnt/etc/modprobe.d/nvidia.conf
 # blacklist nouveau
 options nvidia-drm modeset=1
 options nvidia NVreg_UsePageAttributeTable=1
 EOMODPROBENVIDIACONF
 cat <<EODRACUTCONF >> /mnt/etc/dracut.conf.d/options.conf
-hostonly=yes
+hostonly="yes"
+nofsck="yes"
+add_dracutmodules+=" zfs "
+omit_dracutmodules+=" btrfs "
 EODRACUTCONF
 
 # Some nvidia fixes
@@ -138,33 +154,70 @@ mkdir -p /mnt/udev/rules.d
 chroot /mnt ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
 
 # Install extra packages
-XBPS_ARCH=$ARCH xbps-install -r /mnt -Sy apparmor bluez pipewire gnome gnome-software xdg-user-dirs xdg-user-dirs-gtk xdg-utils xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-gnome cups foomatic-db foomatic-db-nonfree avahi nss-mdns dejavu-fonts-ttf xorg-fonts noto-fonts-ttf noto-fonts-cjk noto-fonts-emoji nerd-fonts autorestic flatpak snapper bash-completion vim 
+XBPS_ARCH=$ARCH xbps-install -r /mnt -Sy NetworkManager apparmor bluez pipewire gnome gnome-software xdg-user-dirs xdg-user-dirs-gtk xdg-utils xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-gnome cups foomatic-db foomatic-db-nonfree avahi nss-mdns dejavu-fonts-ttf xorg-fonts noto-fonts-ttf noto-fonts-cjk noto-fonts-emoji nerd-fonts autorestic flatpak snapper bash-completion vim 
 
 # Enable AppArmor
-sed -i "/GRUB_CMDLINE_LINUX_DEFAULT=/s/\"$/ apparmor=1 security=apparmor&/" /mnt/etc/default/grub
+# sed -i "/GRUB_CMDLINE_LINUX_DEFAULT=/s/\"$/ apparmor=1 security=apparmor&/" /mnt/etc/default/grub
 
 # Configure flatpak
 chroot /mnt flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Install Grub and generate initramfs 
-mount -t efivarfs efivarfs /mnt/sys/firmware/efi/efivars
-chroot /mnt grub-install --target=x86_64-efi --boot-directory=/boot --efi-directory=/boot/efi --bootloader-id="Void Linux" --recheck
+# mount -t efivarfs efivarfs /mnt/sys/firmware/efi/efivars
+# chroot /mnt grub-install --target=x86_64-efi --boot-directory=/boot --efi-directory=/boot/efi --bootloader-id="Void Linux" --recheck
+# chroot /mnt dracut --regenerate-all --force
+# chroot /mnt update-grub
+
+
+# Install and configure ZFSBootMenu and rEFInd
+chroot /mnt zfs set org.zfsbootmenu:commandline="quiet" zroot/ROOT
+mkfs.vfat -F32 "$BOOT_DEVICE"
+cat << EOF >> /etc/fstab
+$( blkid | grep "$BOOT_DEVICE" | cut -d ' ' -f 2 ) /boot/efi vfat defaults 0 0
+EOF
+
+mkdir -p /mnt/boot/efi
+mount /mnt/boot/efi
+
+cat << EOZFSBMCFG > /mnt/etc/zfsbootmenu/config.yaml
+Global:
+  ManageImages: true
+  BootMountPoint: /boot/efi
+Components:
+   Enabled: false
+EFI:
+  ImageDir: /boot/efi/EFI/zbm
+  Versions: false
+  Enabled: true
+Kernel:
+  CommandLine: quiet loglevel=0 apparmor=1 security=apparmor
+EOZFSBMCFG
+
+chroot /mnt generate-zbm  # generate ZFSBootMenu image
+
+refind-install
+rm /mnt/boot/refind_linux.conf
+
+cat << EOF > /mnt/boot/efi/EFI/ZBM/refind_linux.conf
+"Boot default"  "quiet loglevel=0 zbm.skip"
+"Boot to menu"  "quiet loglevel=0 zbm.show"
+EOF
+
 chroot /mnt dracut --regenerate-all --force
-chroot /mnt update-grub
 
 # Install services
-for service in elogind NetworkManager socklog-unix nanoklogd dbus avahi-daemon bluetoothd gdm cupsd grub-btrfs zramen; do
+for service in elogind NetworkManager socklog-unix nanoklogd dbus avahi-daemon bluetoothd gdm cupsd zramen; do
   chroot /mnt ln -sfv /etc/sv/$service /etc/runit/runsvdir/default
 done
 
 # Don't start GDM by default, just in case the video drivers are not working
 touch /mnt/etc/sv/gdm/down
 
-# Set up snapper
-rm -r /mnt/.snapshots
-chroot /mnt snapper -c root create-config /
-btrfs subvolume delete /mnt/.snapshots
-mkdir /mnt/.snapshots
+# # Set up snapper
+# rm -r /mnt/.snapshots
+# chroot /mnt snapper -c root create-config /
+# btrfs subvolume delete /mnt/.snapshots
+# mkdir /mnt/.snapshots
 
 # Change vm.swappiness
 mkdir -p /mnt/etc/sysctl.conf.d
@@ -179,3 +232,9 @@ echo "alias xro='sudo xbps-remove -o'" >> /mnt/home/$USER_NAME/.bashrc
 
 # Reconfigure all packages
 chroot /mnt xbps-reconfigure -fa
+
+# Unmount all filesystems
+umount -n -R /mnt
+
+# Export zpool and prepare for reboot
+zpool export zroot
